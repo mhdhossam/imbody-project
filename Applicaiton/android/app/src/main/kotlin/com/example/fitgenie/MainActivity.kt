f17a057e40a19b38b93bd4abeb2b2e989f36ad3d
moamen-ihab08 <146095726+moamen-ihab08@users.noreply.github.com>
2024-06-08 23:33:04 +0300

    Add files via upload

---
+++ Applicaiton/android/app/src/main/kotlin/com/example/fitgenie/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.fitgenie
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Applicaiton/android/app/src/main/kotlin/com/fitgenie/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.fitgenie
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
